dd7f21a19cf74cfcc8165017b29d9d418285f7a0
FehmiCitiloglu <fehmicitiloglu@gmail.com>
2022-11-08 10:28:33 +0300

    define colors

---
+++ types/valueTypes.ts
@@ -0,0 +1,5 @@
+import {colors} from '../values/colors';
+
+export type ColorsKeys = keyof typeof colors;
+
+export type ColorType = typeof colors[ColorsKeys];
---
+++ values/colors.ts
@@ -0,0 +1,8 @@
+export const colors = {
+  tinder: '#fe3c72',
+  yellow: '#f5d300',
+  white: '#ffffff',
+  blue: '#36C3F8',
+  green: '#21EAB9',
+  purple: '#AF3EF0',
+} as const;
